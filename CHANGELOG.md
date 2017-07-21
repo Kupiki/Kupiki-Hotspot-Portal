9ed2d1fd74b5f7189640f26e9e3a877d0e95b157
Pi Home Server <pihomeserver@gmail.com>
2017-07-21 22:26:42 +0200

    v0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+*** v0.3 ***
+- Issue #5 : Change process to force a reload of the page once the user is connected
+- Small compression of the background image to improve load performances (650Ko instead of 1.1Mo)
+
 *** v0.2 ***
 - New design
 - Management of missing credentials
--- js/portal.js
+++ js/portal.js
@@ -1,12 +1,11 @@
-// Source : https://codepen.io/suez/pen/dPqxoM
+// Inspired from : https://codepen.io/suez/pen/dPqxoM
 
 $(document).ready(function() {
-
     var animating = false,
-        submitPhase1 = 1100,
-        submitPhase2 = 400,
+        firstCheck = true,
+        $spinner = $(".spinner"),
         $login = $(".login"),
-        $hotspot = $(".hotspot"),
+        $connected = $(".connected"),
         $login__submit = $(".login__submit");
 
     chilliController.host = "XXXXXX";
@@ -16,7 +15,13 @@ $(document).ready(function() {
     chilliController.onError = handleErrors;
     chilliController.onUpdate = updateUI;
 
-    var redirectUrl = "http://www.pihomeserver.fr";
+    function handleErrors (code) {
+        alert('The last contact with the Controller failed. Error code =' + code );
+        if (animating) {
+            animating = false;
+            $login__submit.removeClass("processing");
+        }
+    }
 
     function ripple(elem, e) {
         $(".ripple").remove();
@@ -28,6 +33,36 @@ $(document).ready(function() {
         $ripple.css({top: y, left: x});
         elem.append($ripple);
     };
+
+    function connect(username, password) {
+        chilliController.logon( username , password ) ;
+    }
+
+    function updateUI() {
+        if (firstCheck && chilliController.clientState == 1) {
+            // First call and user is already logged in
+            $spinner.hide();
+            $connected.show();
+        } else if (!firstCheck && chilliController.clientState == 1) {
+            // Not first call and user is now logged in
+            location.reload();
+        } else if (chilliController.clientState == 0 && chilliController.command === 'logon') {
+            // User not logged in with error message
+            document.getElementById('login__errors').innerHTML = "<div class='login__row'>" +
+                "<p class='login__signup login__error'>" +
+                "Username or password incorrect" +
+                "</p>" +
+                "</div>";
+
+            animating = false;
+            $login__submit.removeClass("processing");
+        } else {
+            // First check, so display login form
+            firstCheck = false;
+            $spinner.hide();
+            $login.show();
+        }
+    }
 
     $(document).on("click", ".login__submit", function(e) {
         if (animating) return;
@@ -52,50 +87,7 @@ $(document).ready(function() {
         }, 100);
     });
 
-    function redirect() { window.location = redirectUrl; return false; }
-
-    function connect(username, password) {
-        chilliController.logon( username , password ) ;
-    }
-
-    function updateUI() {
-        if (chilliController.clientState == 1) {
-            setTimeout(function() {
-                $login__submit.addClass("success");
-                setTimeout(function() {
-                    $hotspot.show();
-                    $hotspot.css("top");
-                    $hotspot.addClass("active");
-                }, submitPhase2 - 70);
-                setTimeout(function() {
-                    $login.hide();
-                    $login.addClass("inactive");
-                    animating = false;
-                    $login__submit.removeClass("success processing");
-                    //redirect();
-                }, submitPhase2);
-            }, submitPhase1);
-        } else if (chilliController.clientState == 0 && chilliController.command === 'logon') {
-            document.getElementById('login__errors').innerHTML = "<div class='login__row'>" +
-                "<p class='login__signup login__error'>" +
-                "Username or password incorrect" +
-                "</p>" +
-                "</div>";
-
-            animating = false;
-            $login__submit.removeClass("processing");
-
-            return;
-        }
-    }
-
-    function handleErrors (code) {
-        alert('The last contact with the Controller failed. Error code =' + code );
-        if (animating) {
-            animating = false;
-            $login__submit.removeClass("processing");
-        }
-    }
-
+    $spinner.show();
     chilliController.refresh();
+
 });
